0a6735a7ac0df80e9d10461703de572ef34ba602
turi0002 <trui0002@gmail.com>
2017-11-13 17:52:58 +0200

    ad

--- app/src/main/java/com/example/user/ex5/MainActivity.java
+++ app/src/main/java/com/example/user/ex5/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.user.ex5;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RelativeLayout rl= (RelativeLayout)findViewById(R.id.rl1);
+        LinearLayout ll1 = new LinearLayout(this);
+        ll1.setId(R.id.ll1);
+        ll1.setBackground(getResources().getDrawable(R.drawable.border));
+        int width=100;
+
     }
 }
